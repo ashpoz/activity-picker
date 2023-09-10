1b4f8f472af6f4b29c5ded86dfd075014b49bf28
hellomaryjo <majorx3music@gmail.com>
2023-09-10 15:24:33 -0500

    Trying to style pill checkbox in tw

--- src/components/ActivityForm.vue
+++ src/components/ActivityForm.vue
@@ -3,19 +3,16 @@ import { ref } from 'vue'
 import activityFormData from '@/data/activityFormData.json'
 
 let currentStep = ref(activityFormData[0].id)
-// let activityForm = ref(activityFormData)
-// let currentFormData = ref(activityForm.value.find((step) => step.id === currentStep.value)).value
 
 function nextStep() {
   if (currentStep.value === activityFormData.length - 1) return
   currentStep.value++
-  // currentFormData = ref(activityForm.value.find((step) => step.id === currentStep.value))
 }
 </script>
 
 <template>
   <form @submit.prevent="nextStep()">
-    <h2 class="text-4xl font-bold">{{ activityFormData[currentStep].title }}</h2>
+    <h2 class="mb-8 text-4xl font-bold">{{ activityFormData[currentStep].title }}</h2>
     <template v-for="field in activityFormData[currentStep].fields" :key="field[id]">
       <label class="sr-only mb-1" :for="field.id">{{ field.type }}</label>
       <template v-if="field.type === 'input'">
@@ -31,8 +28,16 @@ function nextStep() {
         v-for="option in field.options"
         :key="option.id"
       >
-        <input type="checkbox" :name="option.value" :value="option.value" />
-        <label :for="option.value">{{ option.label }}</label>
+        <input
+          type="checkbox"
+          :name="option.value"
+          :value="option.value"
+          class="hidden"
+          :class="`peer/${option.value}`"
+        />
+        <label :for="option.value" :class="`peer-checked/${option.value}:bg-purple-700`">{{
+          option.label
+        }}</label>
       </template>
       <template v-else-if="field.type === 'select'">
         <select name="" id="" class="mb-4 block w-full border-2 border-black p-2">
@@ -53,35 +58,4 @@ function nextStep() {
       >I don't care, pick for me!</a
     >
   </form>
-  <!-- <h2 class="text-4xl font-bold">{{ step.title }}</h2>
-  <form class="py-8 text-xl" @submit.prevent="nextStep()">
-    <template v-for="field in step.fields" :key="field[id]">
-      <label class="sr-only mb-1" :for="field.id">{{ field.type }}</label>
-      <template v-if="field.type === 'input'">
-        <input
-          class="mb-4 block w-full border-2 border-black p-2"
-          :type="field.type"
-          :id="field.id"
-          :placeholder="field.placeholder"
-        />
-      </template>
-      <template v-else-if="field.type === 'select'">
-        <select name="" id="" class="mb-4 block w-full border-2 border-black p-2">
-          <option value="" disabled selected>Select an option</option>
-          <option v-for="option in field.options" :key="option.id" :value="option.value">
-            {{ option.label }}
-          </option>
-        </select>
-      </template>
-    </template>
-    <button
-      class="mb-4 mt-6 block bg-purple-700 px-10 py-3 font-bold text-white hover:bg-purple-900"
-      type="submit"
-    >
-      Next
-    </button>
-    <a href="" class="text-purple-700 underline hover:text-purple-900"
-      >I don't care, pick for me!</a
-    >
-  </form> -->
 </template>
